d43a0326dc892e2bcbc0a8a3c56fbb9249a69ae5
Jhunna Kumar <jhunnaroy@gmail.com>
2026-06-10 20:19:28 +0530

    ergth

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 // ============================
 app.use(
   cors({
-    origin:  process.env.FRONEND_URL || "http://localhost:5173",
+    origin:  process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
