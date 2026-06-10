4e20d75cc286bbfa41d554a70f42a069fe950141
Jhunna Kumar <jhunnaroy@gmail.com>
2026-06-10 21:12:39 +0530

    fgfh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
-  return <AppRoutes />;
+  return (
+    <>
+    <AppRoutes />;
+    console.log("NEW BUILD");
+console.log(import.meta.env.VITE_API_URL);
+    </>
+
+  )
+  
+  
 };
 
 export default App;
